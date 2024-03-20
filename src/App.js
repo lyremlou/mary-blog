348204954ad278e2b9729322afc2a1c6c90185ec
marysanders <lyremary@yahoo.com>
2024-03-20 15:40:48 -0500

    Add nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
-
+import Navbar from './Navbar';
 function App() {
+  const title="Welcome to the new blog";
+  const likes = 50;
+  const link = "http://www/google.com";
   return (
     <div className="App">
+      <Navbar/>
      <div className="content">
-      <h1>App Component</h1>
+      <h1>{title}</h1>
+      <p>Liked {likes} times!</p>
+      <p>{"Buffy the Vampire Slayer is the best tv show in the world!"}</p>
+      <p>{Math.random() *100}</p>
+      <p>{["apples", "oranges", "bananas"]}</p>
+
+      <a href={link}>Google site </a>
+      
+    
      </div>
     </div>
   );
