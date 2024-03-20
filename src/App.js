acbaf6579f215a8ae2e318050019cf0864918b8e
marysanders <lyremary@yahoo.com>
2024-03-20 17:04:34 -0500

    Edit styles to index.css, remove global App.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import './App.css';
-import Navbar from './Navbar';
-import Home from './Home';
+import Navbar from "./Navbar";
+import Home from "./Home";
 function App() {
-  const title="Welcome to the new blog";
-  const likes = 50;
-  const link = "http://www/google.com";
   return (
     <div className="App">
-      <Navbar/>
-     <div className="content">
-      <Home/>
-      <h1>{title}</h1>
-      <p>Liked {likes} times!</p>
-      <p>{"Buffy the Vampire Slayer is the best tv show in the world!"}</p>
-      <p>{Math.random() *100}</p>
-      <p>{["apples", "oranges", "bananas"]}</p>
-
-      <a href={link}>Google site </a>
-      
-    
-     </div>
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
