d5ed1a36eb1257758efc18191a320c77e5438dd5
marysanders <lyremary@yahoo.com>
2024-03-21 16:23:15 -0500

    Add useState to Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,26 @@
+import {useState} from 'react';
+
 const Home = () => {
+
+    // let name = "Mary"
+    const [name, setName] = useState("Mary");
+    const [age, setAge] = useState(35)
+    
+    const handleClick = () => {
+     setName("Kevin");
+     setAge(44)
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log("hello " + name, e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <button>Click me</button>
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain("Mary", e)}>Click me again</button>
         </div>
      );
 }
