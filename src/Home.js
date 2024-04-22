c576c47e03ea9ab686b442a7898da77696ea2ade
marysanders <lyremary@yahoo.com>
2024-04-22 17:05:24 -0500

    Complete through lesson 18

--- src/Home.js
+++ src/Home.js
@@ -3,29 +3,29 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
-    const [name, setName] = useState('mario');
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        console.log('use effect ran');
-        console.log(name);
-    }, [name]);
+       setTimeout(() => {
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+           return res.json()
+        })
+        .then(data => {
+            console.log(data);
+            setBlogs(data);
+            setIsPending(false);    
+
+        });
+       }, 1000)
+    }, []);
 
   return (
   <div className="home">
-  <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-  <button onClick={() => setName('luigi')}>Change Name</button>
-  <p>{name} </p>
+    { isPending && <div>Loading...</div> }
+  {blogs && <BlogList blogs={blogs} title="All Blogs" />}
   </div>
   );
 };
