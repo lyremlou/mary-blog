9c2d6f0403f8eb6fdf112999593426cff5a72168
marysanders <lyremary@yahoo.com>
2024-04-29 17:23:00 -0500

    Complete Lesson 24

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Create from "./Create";
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/create">
+              <Create />
+            </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
