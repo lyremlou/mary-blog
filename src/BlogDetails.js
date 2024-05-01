474d928e7fba7bbf18e6d4c5ae263b3117bb5741
marysanders <lyremary@yahoo.com>
2024-04-30 19:13:41 -0500

    Complete blog app

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,23 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
 
 const BlogDetails = () => {
     const {id} = useParams();   
     const {data:blog, error, isPending} = useFetch("http://localhost:8000/blogs/" + id);
+    const history = useHistory();
+
+const handleClick = () => {
+    
+
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+        method: 'DELETE'
+    }).then(() => {
+        history.push('/');
+
+    }) 
+}
+
     return (
         <div className="blog-details">
             {isPending && <div>Loading...</div>}
@@ -19,6 +33,7 @@ const BlogDetails = () => {
                     <div>
                         {blog.body}
                     </div>
+                    <button onClick={handleClick}>Delete</button>
                 </article>
             )}
         </div>
